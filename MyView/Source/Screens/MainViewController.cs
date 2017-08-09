66fc7660163e2ca91f06faa28d23fdff93c64485
Kevin Lloyd Baynham <kevin.baynham.k7@gmail.com>
2017-08-09 21:15:55 +0200

    [C] Background image is now selected based on the index from an array of backgrounds instead of a bool.

--- MyView/Source/Screens/MainViewController.cs
+++ MyView/Source/Screens/MainViewController.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Threading.Tasks;
-using Foundation;
 using UIKit;
 
 namespace MyView
@@ -10,14 +9,18 @@ namespace MyView
 	/// </summary>
 	public partial class MainViewController : UIViewController
 	{
+		#region PROPERTIES
+        public int TransitionDuration { get; set; } = 2000;
+		#endregion
+        
+        
 		#region VARIABLES
 		private CategorySelectView m_Select;
 		private HeaderView m_Header;
 		private FooterView m_Footer;
 
-		private bool m_SecondBackgroundActive;
-		
-		
+		private UIImageView[] m_ImageViews;
+		private int m_CurrentImageIdx;
 		#endregion
 
 
@@ -48,7 +51,9 @@ namespace MyView
 			m_Header = BaseView.CreateView<HeaderView>(this.View);
 			m_Footer = BaseView.CreateView<FooterView>(this.View);
 			
-			UIImageBackground1.Alpha = 1f;
+			m_ImageViews = new UIImageView[] { UIImageBackground1, UIImageBackground2 };
+			
+			UIImageBackground1.Alpha = 0f;
 			UIImageBackground2.Alpha = 0f;
 		}
 
@@ -87,29 +92,21 @@ namespace MyView
 		}
 
 		void SetBackground()
-		{
-			UIImageView oldImage, newImage;
-
-			if (!m_SecondBackgroundActive)
-			{
-				oldImage = UIImageBackground1;
-				newImage = UIImageBackground2;
-			}
-			else
-			{
-				oldImage = UIImageBackground2;
-				newImage = UIImageBackground1;
-			}
-
+		{	
+			// Fade out old view		
 			UIView.Animate(
-				1000, 
-				() => { oldImage.Alpha = 0f; }
+				TransitionDuration, 
+				() => { m_ImageViews[m_CurrentImageIdx].Alpha = 0f; }
 			);
-			UIView.Animate(1000, 
-				() => { newImage.Alpha = 1f; }
+			
+			// Increment to next view
+            m_CurrentImageIdx = (++m_CurrentImageIdx % m_ImageViews.Length);
+			
+			// Fade in new view
+			UIView.Animate(
+				TransitionDuration, 
+				() => { m_ImageViews[m_CurrentImageIdx].Alpha = 1f; }
 			);
-
-			m_SecondBackgroundActive = !m_SecondBackgroundActive;
 		}
 		#endregion
 
@@ -117,20 +114,18 @@ namespace MyView
 		#region DEBUG
 		async Task Cycle()
 		{
+			await Task.Delay(3000);
+			ShowImageInterface(true);
+			await Task.Delay(3000);
+			ShowImageInterface(false);
+			
+			await Task.Delay(3000);
+			ShowSelectionInterface(true);
+			await Task.Delay(3000);
+			ShowSelectionInterface(false);
+				
 			for (;;)
 			{
-				await Task.Delay(2000);
-				ShowImageInterface(true);
-
-				await Task.Delay(2000);
-				ShowImageInterface(false);
-				
-				await Task.Delay(2000);
-				ShowSelectionInterface(true);
-				
-				await Task.Delay(2000);
-				ShowSelectionInterface(false);
-				
 				await Task.Delay(2000);
 				SetBackground();
 			}
