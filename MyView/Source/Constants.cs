6f793a2b6eb3bfcd91a92222f3f444a90246ecb6
Kevin Lloyd Baynham <kevin.baynham.k7@gmail.com>
2017-12-07 22:47:37 +0200

    [I] Categories now utilise mulitple keywords in queries for more varied and accurate results.

--- MyView/Source/Constants.cs
+++ MyView/Source/Constants.cs
@@ -34,26 +34,29 @@ namespace MyView
 		{
 			#region PROPERTIES
 			public static SlideshowCategory Random { get; set; } = new SlideshowCategory { SlideshowMode = Mode.Random, DisplayName = "Random" };
-			
+
+			// Important: SlideshowCategory.DisplayName is used to dynamically locate the thumbnail that represents the category. 
+			// If a change is made to the display name, the image resource must be renamed accordingly.
+			// TODO Consider implementing a "SourceName" property to abstract from the display name.
 			public static SlideshowCategory[] Categories { get; set; } = { 
 				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Animals", 		QueryString = "animals" },
 				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Architecture", 	QueryString = "architecture" },
-				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Aviation",		QueryString = "aviation-planes" },
+				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Aviation",		QueryString = "aviation%20planes" },
 				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Bokeh", 			QueryString = "bokeh" },
 				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Cars", 			QueryString = "cars" },
 				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Cities", 		QueryString = "city" },
-				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Food & Drink", 	QueryString = "food-drink-coffee" },
+				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Food & Drink", 	QueryString = "food%20drink%20coffee" },
 				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Motorcycles", 	QueryString = "motorcycles" },
 				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Music", 			QueryString = "music" },
-				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Nature", 		QueryString = "landscape-mountain-forest" },
+				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Nature", 		QueryString = "landscape%20mountain%20forest" },
 				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Objects", 		QueryString = "objects" },
 				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Ocean", 			QueryString = "ocean" },
-				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Office", 		QueryString = "business-office" },
-				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "People", 		QueryString = "love-people-friends" },
+				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Business", 		QueryString = "business%20office" },
+				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "People", 		QueryString = "love%20people%20friends" },
 				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Retro", 			QueryString = "retro" },
 				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Roads", 			QueryString = "roads" },
-				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Seasons", 		QueryString = "winter-spring-autumn" },
-				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Sports", 		QueryString = "extreme-sport" },
+				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Seasons", 		QueryString = "winter%20spring%20autumn" },
+				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Sports", 		QueryString = "extreme%20sport" },
 				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Stars", 			QueryString = "stars" },
 				new SlideshowCategory { SlideshowMode = Mode.Query, 	DisplayName = "Technology", 	QueryString = "technology" },
 			};
